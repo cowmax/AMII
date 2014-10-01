3797cf393faa1ad7f2256f02e6aa32568fab0b70
cowmax <liuzhencheng2@139.com>
2014-10-01 15:22:03 +0800

    finish ExcelDB class. support excel data save to oleDB support oleDB dato save to excel.

--- ImportExcelToDB/ExcelDb.cs
+++ ImportExcelToDB/ExcelDb.cs
@@ -19,56 +19,38 @@ namespace ImportExcelToDB
     /// </summary>
     class ExcelDb
     {
-        DataSet xlsDataSet;
+        DataSet coreDataSet;
         OleDbConnection xlsConnection;
         OleDbDataAdapter xlsDbAdapter;
         string xlsFilePath;
         string xlsSheetName;
-        string dataTableName;
+        string oleDbTableName;
+        int rowCount = 0;
 
         public ExcelDb(DataTable dtTable)
         {
-            this.xlsDataSet = new DataSet();
-            this.xlsDataSet.Tables.Add(dtTable);
-            this.dataTableName = dtTable.TableName;
-            this.xlsSheetName = dtTable.TableName;
+            this.coreDataSet = new DataSet();
+            this.coreDataSet.Tables.Add(dtTable);
+            this.oleDbTableName = dtTable.TableName;
         }
 
         /// <summary>
         /// 构造函数：从Excel文件的指定Sheet 中加载数据到 DataSet 中
         /// </summary>
         /// <param name="xlsFilePath">Excel 文件路径</param>
-        /// <param name="strSheetName">Excel 中的 Sheet 名称</param>
+        /// <param name="sheetName">Excel 中的 Sheet 名称</param>
         /// <param name="srcTableName">数据库集中相应的 Table名称，缺省名称与Sheet 相同</param>
-        public ExcelDb(string xlsFilePath, string strSheetName, string srcTableName = null)
+        public ExcelDb(string xlsFilePath, string sheetName)
         { 
             this.xlsFilePath = xlsFilePath;
-            this.xlsSheetName = strSheetName;
+            this.xlsSheetName = sheetName;
 
-            if (srcTableName == null)
-            {
-                this.dataTableName = GetTableName(strSheetName);
-            }
-            else
-            {
-                this.dataTableName = srcTableName;
-            }
-        }
-
-        // Open the excel file and load 
-        public int Open(string sheetName = null)
-        {
-            this.OpenXlsDbConnection(this.xlsFilePath);
+            this.OpenXlsDbConnection(xlsFilePath);
             this.GetExcelDbAdapter(sheetName);
-            int rowCount = this.FillSheetToDateSet();
-
-            return rowCount;
+            this.rowCount = this.FillSheetToDateSet();
         }
 
-        public void Close()
-        {
-            CloseXlsDbConnection();
-        }
+        #region -- BEGIN: Private functions
 
         private bool isSheetExist(Excel.Sheets sheets, string shtName)
         {
@@ -107,22 +89,194 @@ namespace ImportExcelToDB
             return sht ;
         }
 
+        private string GetCreateSheetSql(string tblName, string[] columnNames)
+        {
+            string sqlCreateTmpl = "CREATE TABLE {0} ({1})";
+
+            string strColumns = null;
+            foreach (string colName in columnNames)
+            {
+                strColumns += "[" + colName + "] VarChar,";
+            }
+
+            strColumns = strColumns.Substring(0, strColumns.Length -1);
+
+            return string.Format(sqlCreateTmpl, tblName, strColumns);
+        }
+
+        private string GetTableName(string sheetName){
+            return sheetName;
+        }
+
+        // Open specified Excel file with OLEDB 
+        private OleDbConnection OpenXlsDbConnection(string xlsFilePath, bool forExport = false)
+        {
+            if (this.xlsConnection == null)
+            {
+                string strConnTmpl = null;
+                if (forExport)
+                {
+                    // 打开 Excel 文件用于创建、写入操作时，参数 IMEX = 1 会导致访问异常
+                    strConnTmpl = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;'";
+                }
+                else
+                {
+                    // NOTE: Different version Excel file need specified connection string
+                    // Excel 2003
+                    // string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + xlsFilePath + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                    // Excel 2007
+                    strConnTmpl = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
+                }
+
+                //连接数据源      
+                this.xlsConnection = new OleDbConnection(string.Format(strConnTmpl, xlsFilePath));
+                this.xlsConnection.Open();
+            }
+
+            return this.xlsConnection;
+        }
+
+        private void CloseXlsDbConnection()
+        {
+            if (this.xlsConnection != null)
+            {
+                this.xlsConnection.Close();
+                this.xlsConnection = null;
+                
+                this.xlsDbAdapter.Dispose();
+                this.xlsDbAdapter = null;
+
+                this.coreDataSet.Dispose();
+                this.coreDataSet = null;
+            }
+        }
+
+        // Fill excel sheet data to DateSet
+        private int FillSheetToDateSet(string tblName = null)
+        {
+            if (tblName != null)
+            {
+                this.oleDbTableName = tblName;
+            }
+            else
+            {
+                this.oleDbTableName = this.xlsSheetName;
+            }
+
+            this.coreDataSet = new DataSet();
+            // Fill data of the table in adapter to specified DataSet with given name
+            return this.xlsDbAdapter.Fill(this.coreDataSet, this.xlsSheetName);
+        }
+
+        private string GetSelectAllSql(string sheetName = null)
+        {
+            if (sheetName != null)
+            {
+                this.xlsSheetName = sheetName;
+            }
+            return ("select * from  [" + this.xlsSheetName + "$] ");
+        }
+
+        // Return an OleDB Adapter for excel file's specified sheet
+        private OleDbDataAdapter GetExcelDbAdapter(string sheetName)
+        {
+            if (this.xlsDbAdapter == null)
+            {
+                string strSql = GetSelectAllSql(sheetName);
+                this.xlsDbAdapter = new OleDbDataAdapter(strSql, this.xlsConnection);
+            }
+
+            return this.xlsDbAdapter;
+        }
+
+        private int Fill(DataSet trgDataSet, string srcTable)
+        {
+            return this.xlsDbAdapter.Fill(trgDataSet, oleDbTableName);
+        }
+
+        private int Export(string filepath, string strSheetName)
+        {
+            this.OpenXlsDbConnection(filepath, true);
+            this.GetExcelDbAdapter(strSheetName);
+            DataTable dt = this.GetOleDbTable();
+            string tblName = this.oleDbTableName;
+            int nRow = 0;
+
+            try
+            {
+                using (OleDbConnection DbConn = this.xlsConnection)
+                {
+                    StringBuilder strSQL = new StringBuilder();
+                    strSQL.Append("CREATE TABLE ").Append("[" + tblName + "]");
+                    strSQL.Append("(");
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        strSQL.Append("[" + dt.Columns[i].ColumnName + "] text,");
+                    }
+                    strSQL = strSQL.Remove(strSQL.Length - 1, 1);
+                    strSQL.Append(")");
+
+                    OleDbCommand cmd = new OleDbCommand(strSQL.ToString(), DbConn);
+                    cmd.ExecuteNonQuery();
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        strSQL.Clear();
+                        StringBuilder strfield = new StringBuilder();
+                        StringBuilder strvalue = new StringBuilder();
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            strfield.Append("[" + dt.Columns[j].ColumnName + "]");
+                            strvalue.Append("'" + dt.Rows[i][j].ToString() + "'");
+                            if (j != dt.Columns.Count - 1)
+                            {
+                                strfield.Append(",");
+                                strvalue.Append(",");
+                            }
+                        }
+                        cmd.CommandText = strSQL.Append(" insert into [" + tblName + "]( ")
+                            .Append(strfield.ToString())
+                            .Append(") values (").Append(strvalue).Append(")").ToString();
+                        nRow = cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return nRow;
+        }
+
+        #endregion -- END :private functions
+
+        public void Close()
+        {
+            CloseXlsDbConnection();
+        }
+
+        public DataTable GetOleDbTable()
+        {
+            return this.coreDataSet.Tables[this.oleDbTableName];
+        }
+
         /// <summary>
-        /// 函数：将DateSet 中的Table 保存到 Excel文件的 Sheet 中
+        /// 函数：使用 Office.Interop.Excel 组件将DateSet 中的Table 保存到 Excel文件的 Sheet 中
         /// </summary>
         /// <param name="srcTableName">DateSet 中 Table 的名称</param>
         /// <param name="xlsFilePath">Excel 文件路径</param>
-        /// <param name="strSheetName">Excel 中 Sheet 的名称</param>
+        /// <param name="sheetName">Excel 中 Sheet 的名称</param>
         /// <returns></returns>
-        public int SaveToXls(string xlsFilePath, string strSheetName)
+        public int SaveToXls(string xlsFilePath, string sheetName)
         {
-            DataTable dtTable = this.xlsDataSet.Tables[this.dataTableName];
+            DataTable dtTable = this.coreDataSet.Tables[this.oleDbTableName];
             int rowCount = dtTable.Rows.Count;
 
             // NOTE : Must Add reference Microsoft.Office.Interop.Excel assembly
             Excel.Application xlsApp = new Excel.Application();
-            Excel.Workbook xlsWrkBook= xlsApp.Application.Workbooks.Open(xlsFilePath);
-            Excel.Worksheet wrkSheet = getSheet(xlsWrkBook.Sheets, strSheetName, true);
+            Excel.Workbook xlsWrkBook = xlsApp.Application.Workbooks.Open(xlsFilePath);
+            Excel.Worksheet wrkSheet = getSheet(xlsWrkBook.Sheets, sheetName, true);
 
             wrkSheet.Visible = Excel.XlSheetVisibility.xlSheetVisible;
             int rowIndex = 1;
@@ -165,225 +319,21 @@ namespace ImportExcelToDB
         }
 
         /// <summary>
-        /// 函数：将DateSet 中的Table 保存到 Excel文件的 Sheet 中
+        /// 函数：使用OLEDB 将DateSet 中的Table 保存到 Excel文件的 Sheet 中
         /// </summary>
         /// <param name="srcTableName">DateSet 中 Table 的名称</param>
         /// <param name="xlsFilePath">Excel 文件路径</param>
         /// <param name="strSheetName">Excel 中 Sheet 的名称</param>
         /// <returns></returns>
-        public int SaveToXlsFile(string xlsFilePath, string strSheetName)
+        public int SaveToXlsFile(string xlsFilePath, string sheetName)
         {
-            DataTable dtTable = this.xlsDataSet.Tables[this.dataTableName];
-            int rowCount = dtTable.Rows.Count;
-
-            OleDbConnection DbConn = this.OpenXlsDbConnection(xlsFilePath);
-            this.GetExcelDbAdapter(strSheetName);
-
-            // Create a new sheet in the opening Excel file with specified name
-            // -- Get column names for the new sheet
-            string [] columnNames = new string[dtTable.Columns.Count];
-            for (int i = 0; i < columnNames.Length; i++){
-                columnNames[i] = dtTable.Columns[i].ColumnName;
-            }
-            // -- Generate a sql for create the new sheet
-            string strCreateSql = GetCreateSheetSql(strSheetName, columnNames);
-
-            // -- Execute the sql with OleDB 
-            OleDbCommand cmd = new OleDbCommand(strCreateSql, DbConn);
-            int effectRows = cmd.ExecuteNonQuery();
-
-            FillSheetToDateSet(strSheetName);
-
-            int rowIndex = 1;
-            int colIndex = 0;
-
-            //// Fill table column name to work-sheet
-            //foreach (DataColumn col in dtTable.Columns)
-            //{
-            //    colIndex++;
-            //    wrkSheet.Cells[rowIndex, colIndex] = col.ColumnName;
-            //}
-
-            //// Fill data-row to work-sheet
-            //foreach (DataRow row in dtTable.Rows)
-            //{
-            //    rowIndex++;
-            //    colIndex = 0;
-            //    foreach (DataColumn col in dtTable.Columns)
-            //    {
-            //        colIndex++;
-            //        wrkSheet.Cells[rowIndex, colIndex] = "'" + row[col.ColumnName].ToString();
-            //    }
-            //}
-
-            //xlsApp.ActiveWorkbook.Save();
-            //xlsApp.Quit();
-            //xlsApp = null;
-            GC.Collect();//垃圾回收
-
-            return rowCount;
+            return Export(xlsFilePath, sheetName);
         }
 
-        /// <summary>
-        /// 方法：将DateSet 的指定 Table 保存到 Excel 文件中
-        /// </summary>
-        /// <param name="ds"></param>
-        /// <param name="FileName"></param>
-        public void CreateExcel(DataSet ds, string tblName, string FileName)
-        {
-            throw (new Exception("This method is not implemented."));
-
-            Page pg = new Page();
-            HttpResponse resp;
-            resp = pg.Response;
-            resp.ContentEncoding = System.Text.Encoding.GetEncoding("GB2312");
-            resp.AppendHeader("Content-Disposition", "attachment;filename=" + FileName);
-            string colHeaders = "", ls_item = "";
-
-            //定义表对象与行对象，同时用DataSet对其值进行初始化 
-            DataTable dt = ds.Tables[tblName];
-            DataRow[] myRow = dt.Select();//可以类似dt.Select("id>10")之形式达到数据筛选目的
-            int i = 0;
-            int cl = dt.Columns.Count;
-
-            //取得数据表各列标题，各标题之间以t分割，最后一个列标题后加回车符 
-            for (i = 0; i < cl; i++)
-            {
-                if (i == (cl - 1))//最后一列，加n
-                {
-                    colHeaders += dt.Columns[i].Caption.ToString() + "n";
-                }
-                else
-                {
-                    colHeaders += dt.Columns[i].Caption.ToString() + "t";
-                }
-
-            }
-            resp.Write(colHeaders);
-            //向HTTP输出流中写入取得的数据信息 
-
-            //逐行处理数据   
-            foreach (DataRow row in myRow)
-            {
-                //当前行数据写入HTTP输出流，并且置空ls_item以便下行数据     
-                for (i = 0; i < cl; i++)
-                {
-                    if (i == (cl - 1))//最后一列，加n
-                    {
-                        ls_item += row[i].ToString() + "n";
-                    }
-                    else
-                    {
-                        ls_item += row[i].ToString() + "t";
-                    }
-
-                }
-                resp.Write(ls_item);
-                ls_item = "";
-
-            }
-            resp.End();
-        }
-
-        private string GetCreateSheetSql(string tblName, string[] columnNames)
-        {
-            string sqlCreateTmpl = "CREATE TABLE {0} ({1})";
-
-            string strColumns = null;
-            foreach (string colName in columnNames)
-            {
-                strColumns += "[" + colName + "] VarChar,";
-            }
-
-            strColumns = strColumns.Substring(0, strColumns.Length -1);
-
-            return string.Format(sqlCreateTmpl, tblName, strColumns);
-        }
-
-        private string GetTableName(string sheetName){
-            return sheetName;
-        }
-
-        // Open specified Excel file with OLEDB 
-        private OleDbConnection OpenXlsDbConnection(string xlsFilePath)
-        {
-            if (this.xlsConnection == null)
-            {
-                // NOTE: Different version Excel file need specified connection string
-                // string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + xlsFilePath + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
-                string strConnTmpl = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;'";
-
-                //连接数据源      
-                this.xlsConnection = new OleDbConnection(string.Format(strConnTmpl, xlsFilePath));
-                this.xlsConnection.Open();
-            }
-
-            return this.xlsConnection;
-        }
-
-        private void CloseXlsDbConnection()
-        {
-            if (this.xlsConnection != null)
-            {
-                this.xlsConnection.Close();
-                this.xlsConnection = null;
-                
-                this.xlsDbAdapter.Dispose();
-                this.xlsDbAdapter = null;
-
-                this.xlsDataSet.Dispose();
-                this.xlsDataSet = null;
-            }
-        }
-
-        // Fill excel sheet data to DateSet
-        private int FillSheetToDateSet(string srcSheet = null)
-        {
-            if (srcSheet != null)
-            {
-                this.xlsSheetName = srcSheet;
-            }
-            this.xlsDataSet = new DataSet();
-
-            // Fill data of the table in adapter to specified DataSet with given name
-            return this.xlsDbAdapter.Fill(this.xlsDataSet, this.dataTableName);
-        }
-
-        private string GetSelectAllSql(string sheetName = null)
-        {
-            if (sheetName != null)
-            {
-                this.xlsSheetName = sheetName;
-            }
-            return ("select * from  [" + this.xlsSheetName + "$] ");
-        }
-
-        // Return an OleDB Adapter for excel file's specified sheet
-        private OleDbDataAdapter GetExcelDbAdapter(string sheetName)
-        {
-            if (this.xlsDbAdapter == null)
-            {
-                string strSql = GetSelectAllSql(sheetName);
-                this.xlsDbAdapter = new OleDbDataAdapter(strSql, this.xlsConnection);
-            }
-
-            return this.xlsDbAdapter;
-        }
-
-        private int Fill(DataSet trgDataSet, string srcTable)
-        {
-            return this.xlsDbAdapter.Fill(trgDataSet, dataTableName);
-        }
-
-        public DataTable GetXlsDbTable()
-        {
-            return this.xlsDataSet.Tables[this.dataTableName];
-        }
-
-        public int AddToDbTable(DataAdapter dtAdapter, DataTable trgDbTable)
+        public int SaveToDb(DataAdapter dtAdapter, DataTable trgDbTable)
         {
             DataRow newRow = null;
-            DataTable srcTable = GetXlsDbTable();
+            DataTable srcTable = GetOleDbTable();
 
             foreach (DataRow srcRow in srcTable.Rows)
             {
@@ -400,12 +350,6 @@ namespace ImportExcelToDB
 
             return dtAdapter.Update(trgDbTable.DataSet);
         }
-
-        public virtual void SaveToDb(DataSet srcDataSet = null)
-        {
-
-        }
-
 
 /*
         protected void Button1_Click(object sender, EventArgs e)
@@ -453,6 +397,7 @@ namespace ImportExcelToDB
             {
             }
             ExcelToDS(Server.MapPath(".") + "\\doc\\" + fileName);
-        }   */
+        }  
+ */
     }
 }
